f8fe9b50a4b474c57674433d0faf237a3d7bb821
Shen Yang <shenyang.dai@gmail.com>
2020-03-24 08:41:41 +0800

    ts

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto"
 	"crypto/rsa"
 	_ "crypto/sha256"
@@ -45,6 +46,67 @@ var (
 
 	// Decrypts a file with a private key
 	decryptionCmd = app.Command("decrypt", "Decrypt from stdin")
+
+	SECRET = `-----BEGIN PGP PRIVATE KEY BLOCK-----
+
+lQEmBF54rVIBCACe2ke4wFIP0E5wNMH1smrl11YUDWf6krklrBNcm4ArlRJ5BsG8
+BDNgstyDDL+VvWpyCH0udDF9yBy3hWM16K6YrrepF29+H5jiPoNmDXmKa2d0vyYc
+1zZDWt/xUpvKmpLhejH5QtnHLbut3CQq5ByleaPtAxtwCSGcPiVbrv5s8ZO17Lmv
++LqKlV7ICpJymp66cn6P0Cgk/83FUhiM13BpJreKCNgKcboC42DwYzWF6Ol+Uxst
+JJ5Ty37j3U5AmDPG/Nkx3njGXcUIaMM+ZXDdXfwGabEQdDi6ZNqagWMkcV7GGSwL
+WYN66lJywzYxldJdlykMk9neR8CJBfOzVC5fABEBAAH/AGUAR05VAhDSdgABJAEC
+AQAGB4QhdgAAtC1TaGVuIFlhbmcgKGpvaW51cykgPHNoZW55YW5nQHRoaW5rb250
+cm9sLmNvbT6JASIEEwEIABYFAl54rVIJEBdR2boAGPcCAhsDAhkBAACTqAf/cTcU
+0lNxdsphxpCeBWWfaNCfU+gdxrjCPwf9JQdnbaL0henufCN2hyzaFwJpzUlvSNqy
+ypyaJNw482eRGykfAr4dK21yjF+Z9+ZFkDAzLh1E0P4+5UeGpwsP+LW9I1t6C+Y9
+AO/EQnWlFByHqONf9QD0TASsOVhkPleuCi/1WZupgXqNmRS8oNoePARayqHd8063
+IENagn3lG6CgPqbot0yt4W2gdP+J00Fwy4ckuxTPaxqZ604178Cw22duq+obWrgd
+QajcbAbHrEsmmrWkN3liwaLEruTnbGJ8uEZIuvRqd3uxZbClNUUD9sxOnAHfxqEL
+p/5989hJbbRgrdCP5J0BJgReeK1SAQgAxiRjcd1BnG9fnjPCLuizzwLE4zA/haRd
+0VIJl+Vo8C5bmtW+O7aeBP0nQI6flEYbzP04R7Q1hZDfqeUJmqimYXJv5iRE5+aq
+0PTSY8DdF/cMWxAq2V4Q6wUzg0whRvqkR6e4CPr9h4h7CGnxlke5Wd54kKK8Fnfl
+lbcotVsmAFkH/R4oTnKEKjmdFLVoYYSJR17keltdaY7mCMtv32dHuczfbvWmqbiW
+NCePSeKuGhSVIrs4ongwtNICAgs0Pyz81/x/eyHq9oHmjp8pTn9Y4aST8dCPWSxB
+kdQ/uORWzY/FwfmG8Y1vb56llkM2XlYTCgenbhM8oxby3h24cV/H4QARAQAB/wBl
+AEdOVQIQ0nYAASQBAgEABgeEIXYAAIkBHwQYAQgAEwUCXnitUgkQF1HZugAY9wIC
+GwwAANFLB/9bElbBMzqxS1ldR4fcZbfaZdARMjjgusBhonGR6goNtX2FHXo2Fvq7
+xBoNLOzW6om2ecWHJ7pbNOSPGuNXVz9knbCg81Nf6T2PZSsZkRKuB7d0IOMqyWdh
+u1ew7ktfteG04YZi2jzILhbH/MdcRfpvB94gI0smGx8640FCUE5AFLb6BIyBZY8X
+2KxABrporFeXZTBV3zep8eXi5I6bYWLiXZHIk73TLVwcC9dJtKfBuu4pA6f6r6nI
+/gTHn/EhBi60z6/HoU7H5TOsT+d5v1KnHyKhMk41ufnfSwYC9erCEeWBzz3vJRs3
+6ujUuTIUUTrnwiQc5uPX2dm5AIpLAF1l
+=6oAH
+-----END PGP PRIVATE KEY BLOCK-----`
+
+	PUB_KEY = `-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+xsBNBF54rVIBCACe2ke4wFIP0E5wNMH1smrl11YUDWf6krklrBNcm4ArlRJ5BsG8
+BDNgstyDDL+VvWpyCH0udDF9yBy3hWM16K6YrrepF29+H5jiPoNmDXmKa2d0vyYc
+1zZDWt/xUpvKmpLhejH5QtnHLbut3CQq5ByleaPtAxtwCSGcPiVbrv5s8ZO17Lmv
++LqKlV7ICpJymp66cn6P0Cgk/83FUhiM13BpJreKCNgKcboC42DwYzWF6Ol+Uxst
+JJ5Ty37j3U5AmDPG/Nkx3njGXcUIaMM+ZXDdXfwGabEQdDi6ZNqagWMkcV7GGSwL
+WYN66lJywzYxldJdlykMk9neR8CJBfOzVC5fABEBAAHNLVNoZW4gWWFuZyAoam9p
+bnVzKSA8c2hlbnlhbmdAdGhpbmtvbnRyb2wuY29tPsLAYgQTAQgAFgUCXnitUgkQ
+F1HZugAY9wICGwMCGQEAAJOoCABxNxTSU3F2ymHGkJ4FZZ9o0J9T6B3GuMI/B/0l
+B2dtovSF6e58I3aHLNoXAmnNSW9I2rLKnJok3DjzZ5EbKR8Cvh0rbXKMX5n35kWQ
+MDMuHUTQ/j7lR4anCw/4tb0jW3oL5j0A78RCdaUUHIeo41/1APRMBKw5WGQ+V64K
+L/VZm6mBeo2ZFLyg2h48BFrKod3zTrcgQ1qCfeUboKA+pui3TK3hbaB0/4nTQXDL
+hyS7FM9rGpnrTjXvwLDbZ26r6htauB1BqNxsBsesSyaataQ3eWLBosSu5OdsYny4
+Rki69Gp3e7FlsKU1RQP2zE6cAd/GoQun/n3z2ElttGCt0I/kzsBNBF54rVIBCADG
+JGNx3UGcb1+eM8Iu6LPPAsTjMD+FpF3RUgmX5WjwLlua1b47tp4E/SdAjp+URhvM
+/ThHtDWFkN+p5QmaqKZhcm/mJETn5qrQ9NJjwN0X9wxbECrZXhDrBTODTCFG+qRH
+p7gI+v2HiHsIafGWR7lZ3niQorwWd+WVtyi1WyYAWQf9HihOcoQqOZ0UtWhhhIlH
+XuR6W11pjuYIy2/fZ0e5zN9u9aapuJY0J49J4q4aFJUiuziieDC00gICCzQ/LPzX
+/H97Ier2geaOnylOf1jhpJPx0I9ZLEGR1D+45FbNj8XB+YbxjW9vnqWWQzZeVhMK
+B6duEzyjFvLeHbhxX8fhABEBAAHCwF8EGAEIABMFAl54rVIJEBdR2boAGPcCAhsM
+AADRSwgAWxJWwTM6sUtZXUeH3GW32mXQETI44LrAYaJxkeoKDbV9hR16Nhb6u8Qa
+DSzs1uqJtnnFhye6WzTkjxrjV1c/ZJ2woPNTX+k9j2UrGZESrge3dCDjKslnYbtX
+sO5LX7XhtOGGYto8yC4Wx/zHXEX6bwfeICNLJhsfOuNBQlBOQBS2+gSMgWWPF9is
+QAa6aKxXl2UwVd83qfHl4uSOm2Fi4l2RyJO90y1cHAvXSbSnwbruKQOn+q+pyP4E
+x5/xIQYutM+vx6FOx+UzrE/neb9Spx8ioTJONbn530sGAvXqwhHlgc897yUbN+ro
+1LkyFFE658IkHObj19nZuQCKSwBdZQ==
+=nRHs
+-----END PGP PUBLIC KEY BLOCK-----`
 )
 
 func main() {
@@ -183,6 +245,32 @@ func encryptFile() {
 	kingpin.Errorf("Encrypted %d bytes", n)
 
 	compressed.Close()
+}
+
+func decryptFile2() {
+	// var entity *openpgp.Entity
+	var entityList openpgp.EntityList
+	entityList, err := openpgp.ReadArmoredKeyRing(bytes.NewBuffer([]byte(SECRET)))
+	if err != nil {
+		log.Println(err)
+	}
+
+	block, err := armor.Decode(os.Stdin)
+	kingpin.FatalIfError(err, "Error reading OpenPGP Armor: %s", err)
+
+	if block.Type != "Message" {
+		kingpin.FatalIfError(err, "Invalid message type")
+	}
+	md, err := openpgp.ReadMessage(block.Body, entityList, nil, nil)
+	kingpin.FatalIfError(err, "Error reading message")
+
+	compressed, err := gzip.NewReader(md.UnverifiedBody)
+	kingpin.FatalIfError(err, "Invalid compression level")
+	defer compressed.Close()
+
+	n, err := io.Copy(os.Stdout, compressed)
+	kingpin.FatalIfError(err, "Error reading encrypted file")
+	kingpin.Errorf("Decrypted %d bytes", n)
 }
 
 func decryptFile() {
